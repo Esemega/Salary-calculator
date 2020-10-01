98f5029e5ee91c920d8bee583d357b313062583d
Esemega <sogaral89@gmail.com>
2020-10-01 20:08:59 +0200

    planning structure

---
+++ app.js
@@ -0,0 +1,14 @@
+//initial data
+const empleado = {
+    bruto: 14500,
+    hijos: 2,
+    pagas: 14
+}
+
+// -------- Calcular Neto anual -----
+//calcular retencion
+
+//restar a retencion si hijos
+
+// -------- Calcular Neto mensual -----
+
